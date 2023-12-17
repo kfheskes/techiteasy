3e3416bcda01c4cf1a8fc28c07f0fa574833446a
kfheskes <kayarneheskes@hotmail.com>
2023-12-17 09:23:30 +0100

    verder gewerkt aan many to many

---
+++ src/main/java/nl/novi/techiteasy/repositories/TelevisionWallBracketRepository.java
@@ -0,0 +1,7 @@
+package nl.novi.techiteasy.repositories;
+
+import nl.novi.techiteasy.models.TelevisionWallBracketKey;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TelevisionWallBracketRepository extends JpaRepository<TelevisionWallBracket, TelevisionWallBracketKey> {
+}
