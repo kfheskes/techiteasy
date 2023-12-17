10260ab704cd3d606855b719bd146431c37e40a7
kfheskes <kayarneheskes@hotmail.com>
2023-12-17 10:26:33 +0100

    uitwerkingen

--- src/main/java/nl/novi/techiteasy/repositories/WallBracketRepository.java
+++ src/main/java/nl/novi/techiteasy/repositories/WallBracketRepository.java
@@ -4,4 +4,6 @@ import nl.novi.techiteasy.models.WallBracket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WallBracketRepository extends JpaRepository <WallBracket, Long> {
+
+
 }
